8699eec1d001a8fa9df5b3d3d33ff799fe742432
karlr <andrew.mikedaniels@gmail.com>
2025-04-06 04:27:49 -0700

    feat: New-SemItemName

---
+++ script/Tool.ps1
@@ -0,0 +1,13 @@
+New-SemItemName {
+    [Alias('Semuid')]
+    Param(
+        [string]
+        $Prefix = 'item',
+
+        [string]
+        $Extension = '.md'
+    )
+
+    return "$($Prefix)_-_$(Get-Date -Format yyyy-MM-dd-HHmmss-fff)$Extension"
+}
+
